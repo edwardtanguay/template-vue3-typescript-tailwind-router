b31f4386bac327241700b8b6c7a1350e3b22c27e
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-30 21:33:04 +0200

    404 in

---
+++ src/pages/PageError404.vue
@@ -0,0 +1,6 @@
+<template>
+	<section class="flex flex-col text-gray-600">
+	<p class="text-[8rem] -mt-[1rem]">404</p>
+	<p class="text-[1.9rem] -mt-[2.4rem]">Page not found</p>
+	</section>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/pages/PageHome.vue'
 import PageAbout from '@/pages/PageAbout.vue'
+import PageError404 from '@/pages/PageError404.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,10 @@ const router = createRouter({
 			path: '/about',
 			name: 'about',
 			component: PageAbout
+		},
+		{
+			path: '/:pathMatch(.*)*',
+			component: PageError404
 		}
 	]
 })
