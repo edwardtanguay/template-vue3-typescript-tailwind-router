697ebff0a74735ef71ada26e273637156efaf8f2
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-30 21:26:50 +0200

    router-link

--- src/components/SiteNav.vue
+++ src/components/SiteNav.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
 </script>
 
 <template>
 	<nav class="bg-gray-400 flex gap-2 px-2 pb-1 pt-1 md:pt-0 rounded mb-3">
-		<RouterLink to="/">Home</RouterLink>
-		<RouterLink to="/about">About</RouterLink>
+		<router-link to="/">Home</router-link>
+		<router-link to="/about">About</router-link>
 	</nav>
 </template>
 
