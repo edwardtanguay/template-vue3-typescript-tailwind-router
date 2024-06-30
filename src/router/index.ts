0c57dbfb07cb7b8b11881ca6b7d46ae867cb442f
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-30 21:24:29 +0200

    changed views to pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PageHome from '@/pages/PageHome.vue'
+import PageAbout from '@/pages/PageAbout.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +8,12 @@ const router = createRouter({
 		{
 			path: '/',
 			name: 'home',
-			component: HomeView
+			component: PageHome
 		},
 		{
 			path: '/about',
 			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import('../views/AboutView.vue')
+			component: PageAbout
 		}
 	]
 })
